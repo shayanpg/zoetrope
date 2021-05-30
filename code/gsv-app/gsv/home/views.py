b6b820a1270bb920364115c2268ea08997814827
Shayan Ghosh <shayan.ghosh@berkeley.edu>
2021-05-30 16:28:35 -0700

    introducing complex html

--- code/gsv-app/gsv/home/views.py
+++ code/gsv-app/gsv/home/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
+
+posts = [
+    {
+        'author': 'SPG',
+        'title': 'P1',
+        'content': 'First',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'JD',
+        'title': 'P2',
+        'content': 'SECOND',
+        'date_posted': 'August 29, 2018'
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1>Homepage</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'home/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>About Page</h1>')
+    return render(request, 'home/about.html', {'title':'About'})
