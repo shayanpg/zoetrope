88bc846b4ab9cfc229f2f79c6df96a6be590c908
Shayan Ghosh <shayan.ghosh@berkeley.edu>
2021-06-20 22:42:07 -0700

    create address, sample apps

--- gsv/home/views.py
+++ gsv/home/views.py
@@ -2,26 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 
-posts = [
-    {
-        'author': 'SPG',
-        'title': 'P1',
-        'content': 'First',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'JD',
-        'title': 'P2',
-        'content': 'SECOND',
-        'date_posted': 'August 29, 2018'
-    }
-]
-
 def home(request):
-    context = {
-        'posts': posts
-    }
-    return render(request, 'home/home.html', context)
+    return render(request, 'home/home.html', {'place':'holder'})
 
 def about(request):
     return render(request, 'home/about.html', {'title':'About'})
