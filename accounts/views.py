2654ce1fedd6dd558132fc44e62482b35f5b20ac
Shayan Ghosh <shayan.ghosh@berkeley.edu>
2021-07-13 00:48:56 -0700

    functional (but ugly) authentication

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
 # Create your views here.
+# from django.shortcuts import render, redirect
+# from django.contrib.auth.forms import UserCreationForm
+# from django.contrib import messages
+#
+# def register(request):
+#     # if request.method == 'POST':
+#     #     form = UserCreationForm(request.POST)
+#     #     if form.is_valid():
+#     #         form.save()
+#     #         username = form.cleaned_data.get('username')
+#     #         messages.success(request, f'Account created for {username}.')
+#     #         return redirect('gsv-home')
+#     # else:
+#     #     form = UserCreationForm()
+#     context = {
+#         'title' : 'New User',
+#         # 'form' : form,
+#     }
+#     return render(request, 'accounts/register.html', context)
+
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+
+from .forms import ProfileCreationForm
+
+class RegisterView(CreateView):
+    form_class = ProfileCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/register.html'
--- gsv/settings.py
+++ gsv/settings.py
@@ -138,5 +138,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.Profile'
 
-# LOGIN_REDIRECT_URL = 'gsv-home'
-# LOGOUT_REDIRECT_URL = 'gsv-home'
+LOGIN_REDIRECT_URL = 'gsv-home'
+LOGOUT_REDIRECT_URL = 'gsv-home'
