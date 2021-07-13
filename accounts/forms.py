73ef5f34f60524d3594ad74466056201f32deb45
Shayan Ghosh <shayan.ghosh@berkeley.edu>
2021-07-13 00:00:21 -0700

    created custom user model

---
+++ accounts/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import Profile
+
+class ProfileCreationForm(UserCreationForm):
+
+    class Meta:
+        model = Profile
+        fields = ('username', 'email', 'gsv_api', 'maps_api')
+
+class ProfileChangeForm(UserChangeForm):
+
+    class Meta:
+        model = Profile
+        fields = ('username', 'email', 'gsv_api', 'maps_api')
--- gsv/urls.py
+++ gsv/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from users import views as user_views
+# from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('address/', include('address.urls')),
     path('sample/', include('sample.urls')),
-    path('register/', user_views.register, name='gsv-register'),
+    # path('register/', user_views.register, name='gsv-register'),
 ]
