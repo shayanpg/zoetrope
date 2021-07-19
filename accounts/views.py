60e19c1e686fe25a8b7659af4e8bf09e14e007fa
Shayan Ghosh <shayan.ghosh@berkeley.edu>
2021-07-18 17:00:39 -0700

    minor fixes/cleanup

--- accounts/views.py
+++ accounts/views.py
@@ -1,29 +1,5 @@
-# from django.shortcuts import render
-
-# Create your views here.
-# from django.shortcuts import render, redirect
-# from django.contrib.auth.forms import UserCreationForm
-# from django.contrib import messages
-#
-# def register(request):
-#     # if request.method == 'POST':
-#     #     form = UserCreationForm(request.POST)
-#     #     if form.is_valid():
-#     #         form.save()
-#     #         username = form.cleaned_data.get('username')
-#     #         messages.success(request, f'Account created for {username}.')
-#     #         return redirect('home')
-#     # else:
-#     #     form = UserCreationForm()
-#     context = {
-#         'title' : 'New User',
-#         # 'form' : form,
-#     }
-#     return render(request, 'accounts/register.html', context)
-
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
-
 from .forms import ProfileCreationForm
 
 class RegisterView(CreateView):
