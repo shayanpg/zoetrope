9196e4c6522b9fe30527a5ff3b4124a7a7d9c284
Shayan Ghosh <shayan.ghosh@berkeley.edu>
2021-07-18 17:28:13 -0700

    login_required on sample

--- sample/views.py
+++ sample/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
-
+@login_required
 def sample(request):
     return render(request, 'sample/sample.html', {'title':'Neighborhood Sampler'})
