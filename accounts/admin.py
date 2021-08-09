cd2378c474f5455a405f15930d25301f3c1403f3
Shayan Ghosh <shayan.ghosh@berkeley.edu>
2021-08-08 18:41:56 -0700

    profile updater

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .forms import ProfileCreationForm, ProfileChangeForm
+from .forms import ProfileCreationForm, ProfileUpdateForm
 from .models import Profile
 
 class ProfileAdmin(UserAdmin):
     add_form = ProfileCreationForm
-    form = ProfileChangeForm
+    form = ProfileUpdateForm
     model = Profile
     list_display = ['email', 'username', 'gsv_api', 'maps_api']
 
--- accounts/forms.py
+++ accounts/forms.py
@@ -24,12 +24,27 @@ class ProfileCreationForm(UserCreationForm):
 
     class Meta:
         model = Profile
-        fields = ('username', 'email', 'gsv_api', 'maps_api')
+        fields = ['username', 'email', 'gsv_api', 'maps_api']
 
-class ProfileChangeForm(UserChangeForm):
-    # def __init__(self, *args, **kwargs):
-    #     super().__init__(*args, **kwargs)
+class ProfileUpdateForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['gsv_api'].required = True
+        self.fields['maps_api'].required = True
+        self.fields['email'].required = True
+
+        self.fields['gsv_api'].label = "Google Street View API Key"
+        self.fields['maps_api'].label = "Google Maps API Key"
+
+        self.fields['gsv_api'].help_text = mark_safe(
+            "Don't have an API key? Learn how to get one " +
+            "<a href=https://developers.google.com/maps/documentation/streetview/get-api-key target=_blank>here</a>."
+        )
+        self.fields['maps_api'].help_text = mark_safe(
+            "Don't have an API key? Learn how to get one " +
+            "<a href=https://developers.google.com/maps/documentation/geocoding/start target=_blank>here</a>."
+        )
 
     class Meta:
         model = Profile
-        fields = ('username', 'email', 'gsv_api', 'maps_api')
+        fields = ['username', 'email', 'gsv_api', 'maps_api']
