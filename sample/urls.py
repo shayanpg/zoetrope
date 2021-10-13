4e3ffc46476222c995847077ab49d1b5d543d524
Shayan Ghosh <shayan.ghosh@berkeley.edu>
2021-10-13 15:40:02 -0700

    temp integration of map app into primary

--- sample/urls.py
+++ sample/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+# TEMP: using map app index file, delete later
+from map import views as mviews
+
 urlpatterns = [
-    path('', views.sample, name='sample'),
+    # path('', views.sample, name='sample'),
+    # TEMP: Delete following path later, uncomment last line
+    path('', mviews.index, name='sample'),
 ]
