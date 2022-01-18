3bbb49d3ba92da9824dcd7c6f1000f04c47f21a9
Shayan Ghosh <shayan.ghosh@berkeley.edu>
2022-01-17 18:04:57 -0800

    temporarily changing secret keys to randomly regenerate - THIS IS TEMPORARY - will not let you stay logged in

--- gsv/settings.py
+++ gsv/settings.py
@@ -19,9 +19,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
+from django.core.management.utils import get_random_secret_key
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['ZOE_SECRET_KEY']
+
+# NOTE: In production use environment variables and don't randomly generate a key
+# SECRET_KEY = os.environ['ZOE_SECRET_KEY']
+SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ['DEBUG'] == 'True')
