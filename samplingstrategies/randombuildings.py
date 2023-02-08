095b547858816f61a0123f49e16e8559523e6f5f
Shayan Ghosh <shayan.ghosh@berkeley.edu>
2023-02-07 18:03:47 -0800

    samplingstrategies: fill out randombuildings CONFIG dictionary

--- samplingstrategies/randombuildings.py
+++ samplingstrategies/randombuildings.py
@@ -1,9 +1,18 @@
 from .strategy import SamplingStrategy
 
+from neighborhood.models import Neighborhood
+from accounts.models import Profile
+
 class RandomBuildings(SamplingStrategy):
     NAME = 'Random Buildings'
 
-    CONFIG = {'name':[str, NAME]}
+    CONFIG = {
+        'name':[str, NAME],
+        'num_points': [int, "Number of points to sample"],
+        'neighborhood': [Neighborhood, "Neighborhood model to sample"],
+        'tolerance': [int, "How many retries should be allowed for points without image results"],
+        'user': [Profile, "Representation of the calling User"],
+        }
     CONFIG = {**(SamplingStrategy.CONFIG), **CONFIG} # Include entries from SamplingStrategy, **CONFIG is second arg to override super()
 
     def sample(self, request): # request is JSON with same keys as config, with values being actual value instances
